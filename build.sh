135b59e3550348f0209b1e701b7fe6ec35b71fb5
Blackskyliner <Blackskyliner@users.noreply.github.com>
2019-01-07 19:34:28 +0100

    Cleaned build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# This function will take: 
+#   - the working directory of a docker command
+#   - the name of the dockerfile
+#   - the tag which the built image should get
+#   - an arbitrary list of arguments for docker build
+# and build the corresponding docker image.
 buildImage(){
     FOLDER=$1
     shift
@@ -10,12 +16,14 @@ buildImage(){
     TAG=$1
     shift
 
+    # pushd/popd for /bin/sh
     OLDPWD=$(pwd)
     cd "$FOLDER"
+        # Build and tag the file
         docker build --tag="$TAG" --file "$DOCKERFILE" "$@" .
     cd "$OLDPWD"
 }
 
 # Base Images (latest and centos7 are rolling release versions)
-buildImage . Dockerfile base --no-cache --build-arg DOCKER_CENTOS_VERSION=latest
-buildImage . Dockerfile base:centos-7 --build-arg DOCKER_CENTOS_VERSION=7
+buildImage    .    Dockerfile    base/centos:latest    --build-arg DOCKER_CENTOS_VERSION=latest
+buildImage    .    Dockerfile    base/centos:7         --build-arg DOCKER_CENTOS_VERSION=7
